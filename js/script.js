76015e73aa4823e0ae1e7c076b962d81790d3ed8
kristofferhebert <kristofferhebert>
2018-06-17 15:24:16 -0700

    Adding bootstrap function

--- js/script.js
+++ js/script.js
@@ -1,35 +1,44 @@
+(function(){
 
-// Create window popup based on link
-function triggerPopup(link){
-    return function(e){
-        e.preventDefault();
-        var page = link.href.split('#').pop();
-        
-        // Create new or update existing popup
-        if (popup != false){
-            popup.location.replace(page);
-        } else {
-            popup = window.open(page, "Popup", 'location=yes,height=500,width=500,scrollbars=yes,status=yes');
+    // Create window popup based on link
+    function triggerPopup(link){
+        return function(e){
+            e.preventDefault();
+            var page = link.href.split('#').pop();
+            
+            // Create new or update existing popup
+            if (popup != false){
+                popup.location.replace(page);
+            } else {
+                popup = window.open(page, "Popup", 'location=yes,height=500,width=500,scrollbars=yes,status=yes');
+            }
         }
     }
-}
 
-function closePopup(e){
-    e.preventDefault();
-    popup.close();
-}
-
-// Cache current popup window
-var popup = false;
-
-// Get all Menu links
-var menuLinks = document.querySelectorAll('li a');
-
-for(var i = 0; i < menuLinks.length; i++){
-    if(menuLinks[i].href.indexOf('close') !== -1){
-        menuLinks[i].addEventListener("click", closePopup, true);
-    } else {
-        menuLinks[i].addEventListener("click", triggerPopup(menuLinks[i]), true);
-
+    // Used to close Window popup when user clicks close
+    function closePopup(e){
+        e.preventDefault();
+        popup.close();
     }
-}
+
+    function bootstrap() {
+        // Cache current popup window
+        var popup = false;
+
+        // Get all Menu anchor tags
+        var menuLinks = document.querySelectorAll('li a');
+
+        // Assign on click events
+        for(var i = 0; i < menuLinks.length; i++){
+            if(menuLinks[i].href.indexOf('close') !== -1){
+                menuLinks[i].addEventListener("click", closePopup, true);
+            } else {
+                menuLinks[i].addEventListener("click", triggerPopup(menuLinks[i]), true);
+
+            }
+        }
+    }
+
+    window.onload = bootstrap;
+
+})();
